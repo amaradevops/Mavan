bfd954afe075c9220fa506163736dcf9825a92c5
amaradevops1234 <amaradevops1234@gmail.com>
2020-03-08 09:30:31 +0530

    second commit

--- src/main/java/com/icicitech/App.java
+++ src/main/java/com/icicitech/App.java
@@ -1,13 +1,13 @@
 package com.icicitech;
 
 /**
- * Hello world!
+ * Hello world Amara!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Amara" );
     }
 }
